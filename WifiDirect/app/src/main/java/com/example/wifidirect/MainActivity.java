b6e0fb621372a04ae1a5091bf5d4475f2699b5fb
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-04-06 20:05:28 -0400

    added button functionality to turn on/off wifi N.B. use target SDK=28 as this function is depreciated

--- WifiDirect/app/src/main/java/com/example/wifidirect/MainActivity.java
+++ WifiDirect/app/src/main/java/com/example/wifidirect/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.wifidirect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private TextView read_msg_box, connectionStatus;
     private EditText writeMsg;
+    WifiManager wifiManager;
 
 
     @Override
@@ -21,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialWork();
+        exqListener();
+    }
+
+    private void exqListener() {
+        btnOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(wifiManager.isWifiEnabled())
+                {
+                    wifiManager.setWifiEnabled(false);
+                    btnOnOff.setText("Turn ON");
+                }
+                else {
+                    wifiManager.setWifiEnabled(true);
+                    btnOnOff.setText("Turn OFF");
+                }
+            }
+        });
     }
 
     private void initialWork() {
@@ -31,5 +53,9 @@ public class MainActivity extends AppCompatActivity {
         read_msg_box=(TextView) findViewById(R.id.readMsg);
         connectionStatus=(TextView) findViewById(R.id.connectionStatus);
         writeMsg = (EditText) findViewById(R.id.writeMsg);
+
+        wifiManager =(WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+
     }
 }
